dbb64b12cb1ee5a797f642153f2857c1d18a141e
Jake Luer <jake.luer@gmail.com>
2011-12-05 08:29:34 -0500

    compatible with match 0.2.0

--- benchmarks/comparison.js
+++ benchmarks/comparison.js
@@ -1,5 +1,3 @@
-var matcha = require('matcha');
-
 var EE = require('events').EventEmitter
   , ee = new EE();
 
@@ -20,50 +18,44 @@ var drop2 = new drip({
   wildcards: true
 });
 
-var suite = new matcha.Suite({
-  iterations: 10000
-});
-
-suite.bench('events heating up', function (next) {
+bench('events heating up', function (next) {
   ee.on('test1', function () { 1==1; });
   ee.emit('test1');
   ee.removeAllListeners('test1');
   next();
 });
 
-suite.bench('nodejs native events', function (next) {
+bench('nodejs native events', function (next) {
   ee1.on('test1', function () { 1==1; });
   ee1.emit('test1');
   ee1.removeAllListeners('test1');
   next();
 });
 
-suite.bench('eventemitter2 standard events', function (next) {
+bench('eventemitter2 standard events', function (next) {
   ee2.on('test2', function () { 1==1; });
   ee2.emit('test2');
   ee2.removeAllListeners('test2');
   next();
 });
 
-suite.bench('eventemitter2 wildcard events', function (next) {
+bench('eventemitter2 wildcard events', function (next) {
   ee2wc.on('test2.foo', function () { 1==1; });
   ee2wc.emit('test2.foo');
   ee2wc.removeAllListeners('test2.foo');
   next();
 });
 
-suite.bench('drip standard events', function (next) {
+bench('drip standard events', function (next) {
   drop.on('test3', function () { 1==1; });
   drop.emit('test3');
   drop.removeAllListeners('test3');
   next();
 });
 
-suite.bench('drip wildcard events', function (next) {
+bench('drip wildcard events', function (next) {
   drop2.on('test3:foo', function () { 1==1; });
   drop2.emit('test3:foo');
   drop2.off('test3:foo');
   next();
 });
-
-module.exports = suite;
--- benchmarks/scenarios.js
+++ benchmarks/scenarios.js
@@ -1,9 +1,3 @@
-var matcha = require('matcha');
-
-var suite = new matcha.Suite({
-  iterations: 100
-});
-
 var drip = require('../lib/drip')
   , drop = new drip();
 
@@ -13,25 +7,23 @@ var EE = require('events').EventEmitter
 var EE2 = require('eventemitter2').EventEmitter2
   , ee2 = new EE2();
 
-suite.bench('drip on/off single', function (next) {
+bench('drip on/off single', function (next) {
   drop = new drip();
   drop.on('test', function () { 1 == 1; });
   drop.removeAllListeners('test');
   next();
 });
 
-suite.bench('ee on/off single', function (next) {
+bench('ee on/off single', function (next) {
   ee = new EE();
   ee.on('test', function () { 1 == 1; });
   ee.removeAllListeners('test');
   next();
 });
 
-suite.bench('ee2 on/off single', function (next) {
+bench('ee2 on/off single', function (next) {
   ee2 = new EE2();
   ee2.on('test', function () { 1 == 1; });
   ee2.removeAllListeners('test');
   next();
 });
-
-module.exports = suite;
