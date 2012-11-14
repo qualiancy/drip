fcb22c2fde792b2899d0f1ebee276b2733ccad25
Jake Luer <jake.luer@gmail.com>
2012-11-14 11:52:57 -0500

    add component.json

---
+++ component.js
@@ -0,0 +1,21 @@
+{
+    "name": "drip"
+  , "repo": "qualiancy/drip"
+  , "version": "0.3.1"
+  , "description": "An EventEmitter alternative for nodejs and the browser that supports namespaces and wildcards."
+  , "license": "MIT"
+  , "keywords": [
+        "event"
+      , "events"
+      , "eventemitter"
+      , "emitter"
+    ]
+  , "main": "lib/drip.js"
+  , "scripts": [
+        "lib/drip.js"
+    ]
+  , "dependencies": {
+        "qualiancy/tea-concat": "0.1.0"
+    }
+  , "development": {}
+}
