3d1dfc65261a6417ddaf2aa6d4da28866e5420ec
Jayson Harshbarger <hypercubed@gmail.com>
2016-05-24 19:47:30 +0900

    Fix for multiple listeners, 3+ arguments

--- test/emitter.emit.js
+++ test/emitter.emit.js
@@ -55,15 +55,19 @@ describe('emitter', function () {
   });
 
   describe('.emit(\'event\', arg, arg, arg, arg, arg)', function () {
-    it('should trigger listeners with 3+ arguments', function () {
+    it('should trigger multiple listeners with 3+ arguments', function () {
       var emitter = new drip.EventEmitter()
         , listener = chai.spy()
+        , listener2 = chai.spy()
         , noop = chai.spy();
       emitter.on('event', listener);
+      emitter.on('event', listener2);
       emitter.on('noop', noop);
       emitter.emit('event', 1, 2, 'three', 4, 5);
       listener.should.have.been.called.once;
       listener.should.have.always.been.called.with.exactly(1, 2, 'three', 4, 5);
+      listener2.should.have.been.called.once;
+      listener2.should.have.always.been.called.with.exactly(1, 2, 'three', 4, 5);
       noop.should.not.have.been.called();
     });
   });
